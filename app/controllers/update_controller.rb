b513030cd4893f5c0ab4863dd8999ac3ff5aeb19
abeylann <alicia.navarrete@deliveroo.co.uk>
2016-08-08 17:36:08 +0100

    modiffied if/else

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -7,9 +7,7 @@ def up
 	respond_to do |f|
 		f.json {render :json => @info}
 
-		# I know that it IS checking existance by issueID... And it _is_ doing what it
-		# is supposed to do. But for some reason, it prints ! and . in every line of 
-		# params... and makes no much sense because of that. 
+		
 
 		JSON.parse(request.body.read).each do |item|
 			@issueType = params[:fields]["issuetype"]["id"]
@@ -17,15 +15,12 @@ def up
 			i = Info.new(:issueType => (@issueType.to_i), :issueID => (@issueID.to_i))
 			if Info.exists?(:issueID => @issueID.to_i)
 					puts "It Exists"
-					if Info.where(:issueID == @issueID.to_i)&&(:issueType == @issueType.to_i)
-						puts "It's the same"
-					else
+					if Info.where(:issueID == @issueID.to_i)&&(:issueType != @issueType.to_i)
 						puts "Has changed"
-						# i.delete
-						# if Info.where (:issueID == @issueID.to_i) do 
 						Info.where(:issueID => @issueID.to_i).update(issueType: @issueType.to_i)
 						i.save
-						
+					else
+						puts "It's the same"
 					end
 					
 			else
