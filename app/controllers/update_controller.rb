eb0ba9df572e1eb6b9960b393077bbe36989ff89
abeylann <alicia.navarrete@deliveroo.co.uk>
2016-08-31 15:38:44 +0100

    Created separate methods

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -1,7 +1,6 @@
 require 'slack-notifier'
 require 'pagerduty'
 class UpdateController < ApplicationController
-	include ActiveModel::Dirty
 	skip_before_action :verify_authenticity_token
 
 def up
@@ -17,11 +16,10 @@ def up
 			@type = params[:issue]["fields"]["issuetype"]["name"]
 			@status = params[:issue]["fields"]["status"]["name"]
 			@priority = params[:issue]["fields"]["priority"]["name"]
-			url = ENV["JIRA_URL"]
-			slack_new = ENV["SLACK_NEW"]
+			@url = ENV["JIRA_URL"]
 			slack_update = ENV["SLACK_UPDATE"]
-			link = "\n For more info, click here: "+ url + @name
-			notifier = Slack::Notifier.new slack_update, channel: '#jiraslack', username: 'Incident Updates'
+			@notifier_up = Slack::Notifier.new slack_update, channel: '#jiraslack', username: 'Incident Updates'
+			@link = "\n For more info, click here: "+ @url + @name
 			inf = Info.new(:issueType => (@issueType.to_i), :issueID => (@issueID.to_i))
 
 			if Info.exists?(:issueID => @issueID.to_i)
@@ -31,41 +29,54 @@ def up
 
 							if issue.issueType == 10300 && @issueType == "10004"
 								 issue.update(issueType: @issueType)
+								 downgraded(inf)
 								# incident = pagerduty.get_incident(@issueID)
 								# incident.resolve
-								attachment1 = {
-									fallback: "Emergency downgraded to bug",
-									text: "<!channel> \nIncident " + @name + " has now been downgraded to Bug" + link,
-									color: "#0078ff"
-								}
-								notifier.ping attachments:  [attachment1]
-
 							elsif issue.issueType == 10004 && @issueType == "10300"
 								issue.update(issueType: @issueType)
+								upgrade(inf)
 								# incident2 = pagerduty.trigger(@description)
-								attachment2 = {
-									fallback:  "Bug upgraded to Emergency",
-									text: "<!channel> \nIncident "+ @name +" has now been upgraded to Emergency" + link,
-									color: "#FF0000"
-								}
-								notifier.ping attachments: [attachment2]
 							end
-				end
+						end
 
 			else
 			inf.save
 			if inf.issueType == 10300
-				attachment_new = {
-							fallback: "New emergency",
-							text: "<!channel>" +"\n Type: " + @type + "\n Incident: " + @name + "\n Summary: "+ @description+"\n Status: "+ @status +"\n Priority: "+ @priority + link,
-							color: '#ff0000'
-						}
-            notifier = Slack::Notifier.new slack_new, channel: '#jiraslack', username: 'Incident'
-            notifier.ping attachments: [attachment_new]
+				created(inf)
 			end
 			end
-
 	end
-
 end
+
+private
+
+def upgrade(inf)
+	attachment = {
+		fallback:  "Bug upgraded to Emergency",
+		text: "<!channel> \nIncident "+ @name +" has now been upgraded to Emergency" + @link,
+		color: "#FF0000"
+	}
+	@notifier_up.ping attachments: [attachment]
+end
+
+def downgraded (inf)
+	attachment1 = {
+		fallback: "Emergency downgraded to bug",
+		text: "<!channel> \nIncident " + @name + " has now been downgraded to Bug" + @link,
+		color: "#0078ff"
+	}
+	@notifier_up.ping attachments:  [attachment1]
+end
+
+def created(inf)
+	slack_new = ENV["SLACK_NEW"]
+	attachment_new = {
+				fallback: "New emergency",
+				text: "<!channel>" +"\n Type: " + @type + "\n Incident: " + @name + "\n Summary: "+ @description+"\n Status: "+ @status +"\n Priority: "+ @priority + @link,
+				color: '#ff0000'
+			}
+			notifier = Slack::Notifier.new slack_new, channel: '#jiraslack', username: 'Incident'
+			notifier.ping attachments: [attachment_new]
+end
+
 end
--- app/models/info.rb
+++ app/models/info.rb
@@ -2,25 +2,5 @@ class Info < ApplicationRecord
 
 	validates :issueID, :uniqueness => true
 	
-	# infos = Info.all
 
-	# if Info.exists?(:issueID => @issueID)
-	# 	puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	# else
-	# 	puts "...................................."
-	# 	Info.create
-	# end
-
-
-	# end
-		# def as_json (options= {})
-		# response = JSON.parse(request.body.read)
-
-		# response['fields'][1...-1].each do |data|
-		# 	# Info.create(
-		# 	# 	:issueID => params[:fields]["id"].to_i,
-		# 	# 	:issueType => params[:fields]["issuetype"]["id"].to_i
-		# 	# 	)
-		# 	# Info.save
-		# end
-end		# end
+end		#
